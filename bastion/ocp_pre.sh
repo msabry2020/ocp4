6339afc99bec162bec4b1aee43abd84be79f0a3a
Mohammed_Sabry <mohammed.sabry@ibm.com>
2023-08-26 15:41:07 +0300

    Adding Variables to Infra scripts

--- bastion/ocp_pre.sh
+++ bastion/ocp_pre.sh
@@ -13,7 +13,10 @@ tar xvf openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
 tar xvf openshift-install-linux-$OCP_VERSION.tar.gz -C /usr/bin/
 
 # Download the OpenShift images
-openshift-install coreos print-stream-json | grep -Eo '"https.*(kernel-|initramfs.|rootfs.)\w+(\.img)?"' | grep x86 | xargs wget -P images
+mkdir images
+openshift-install coreos print-stream-json | grep -Eo '"https.*(kernel-)\w+(\.img)?"' | grep x86 | xargs wget -O images/rhcos-live-kernel-x86_6
+openshift-install coreos print-stream-json | grep -Eo '"https.*(initramfs.)\w+(\.img)?"' | grep x86 | xargs wget -O images/rhcos-live-initramfs.x86_64.img
+openshift-install coreos print-stream-json | grep -Eo '"https.*(rootfs.)\w+(\.img)?"' | grep x86 | xargs wget -O images/rhcos-live-rootfs.x86_64.img
 
 # Copy the OpenShift images to the infra node
 rsync -av images infra:/var/www/html/openshift4/
