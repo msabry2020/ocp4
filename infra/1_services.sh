24ea840d9fd4a3246be2699454f3be31bd0e919b
root <root@infra-nbe1.fyre.ibm.com>
2023-08-30 06:01:26 -0700

    adding more visibilty to the services script

--- infra/1_services.sh
+++ infra/1_services.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Set the variables
+echo '# Set the variables #'
 BASE_DOMAIN='nbe.ahly.bank'
 CLUSTER_NAME='plz-vmware-sit-c01'
-SUBNET='xxx.yyy.zzz'
-REVERSE_SUBNET='xxx.yyy.zzz'
-INFRA_IP="${SUBNET}.abc"
-REGISTRY_IP="${SUBNET}.xyz"
+SUBNET='192.168.100'
+REVERSE_SUBNET='100.168.192'
+INFRA_IP="${SUBNET}.100"
+REGISTRY_IP="${SUBNET}.105"
 LB_OCT=$(echo $INFRA_IP | awk -F. '{print $4}')
 
-# Backup existing config files
+echo '# Backup existing config files #'
+set -x
 cp /etc/named.conf /etc/named.conf.bkp
 cp /var/named/${BASE_DOMAIN}.zone /var/named/${BASE_DOMAIN}.zone.bkp
 cp /var/named/${BASE_DOMAIN}.reverse.zone /var/named/${BASE_DOMAIN}.reverse.zone.bkp
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# DNS #
+echo '# DNS Configuration #'
+set -x
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" named.conf 
 sed -i "s/SUBNET/${SUBNET}/g" forward.zone 
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" forward.zone
@@ -24,25 +29,35 @@ sed -i "s/REGISTRY_IP/${REGISTRY_IP}/g" forward.zone
 sed -i "s/LB_OCT/$LB_OCT/g" reverse.zone
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" reverse.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" reverse.zone
+sed -i "s/REVERSE_SUBNET/${REVERSE_SUBNET}/g" reverse.zone
 cp named.conf /etc/named.conf
 cp forward.zone /var/named/${BASE_DOMAIN}.zone 
 cp reverse.zone /var/named/${BASE_DOMAIN}.reverse.zone
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# HAProxy #
+echo '# HAProxy Configuration #'
+set -x
 sed -i "s/SUBNET/${SUBNET}/g" haproxy.cfg
 cp haproxy.cfg /etc/haproxy/haproxy.cfg 
-#semanage boolean -m --on haproxy_connect_any
-#setsebool -P haproxy_connect_any=1
+semanage boolean -m --on haproxy_connect_any
+setsebool -P haproxy_connect_any=1
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# HTTPD # 
+echo '# HTTP Server Configuration #'
+set -x
 cp httpd.conf /etc/httpd/conf/httpd.conf
 mkdir -p /var/www/html/openshift4/ignitions
 restorecon -Rv /var/www/html/openshift4
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# Start and Enable Services #
-systemctl enable --now dhcpd
+echo # Start and Enable Services #
+set -x
 systemctl enable --now named
-systemctl enable --now tftp
 systemctl enable --now haproxy
 systemctl enable --now httpd
-
