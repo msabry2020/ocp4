6759ca40669bc26a2506642005927d869a454658
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2023-09-19 15:57:14 +0300

    Update 3_mirror_images.sh

--- bastion/3_mirror_images.sh
+++ bastion/3_mirror_images.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Check if the script was called with an argument
-if [ $# -eq 0 ]; then
-  echo 'Error: No argument passed'
-  echo 'Please pass the init password for the private registry'
-  exit 1
-fi
-
 # Set the variables
-TOKEN="$1"
 OCP_RELEASE='4.10.66'
 LOCAL_REGISTRY='registry.plz-vmware-sit-c01.nbe.ahly.bank:8443'
 LOCAL_REPOSITORY=plz-vmware-sit-c01/openshift4
@@ -16,21 +8,19 @@ PRODUCT_REPO='openshift-release-dev'
 RELEASE_NAME="ocp-release"
 LOCAL_SECRET_JSON="${HOME}/pull-secret.json"
 ARCHITECTURE=x86_64
+REGISTRY_TOKEN=$(echo -n "init:${INIT_PASSWORD}" | base64 -w0)
+LOCAL_REGISTRY_SECRET="auths\":{\"registry.${CLUSTER_NAME}.${BASE_DOMAIN}:8443\":{\"auth\":\"${REGISTRY_TOKEN}\",\"email\":\"admin@${BASE_DOMAIN}\"},"
+
 
 # Log in to the registry as the init user
-podman login -u init -p $TOKEN $LOCAL_REGISTRY 
+podman login -u init -p $INIT_PASSWORD $LOCAL_REGISTRY 
 
-# Get the pull secret
-cat pull-secret | jq . > $LOCAL_SECRET_JSON
+
 
 # Convert the pull secret to base64
-echo -n "init:${TOKEN}" | base64 -w0
+echo -n "init:${INIT_PASSWORD}" | base64 -w0
 
-# Sleep 10 seconds to copy the secret
-sleep 10
-
-# Open the pull secret file in vim
-vim $LOCAL_SECRET_JSON 
+sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" pull-secret > $LOCAL_SECRET_JSON
 
 oc adm release mirror -a ${LOCAL_SECRET_JSON} --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE}
 
