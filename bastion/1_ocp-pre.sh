bf82da3adf93b141791480aa901ceb072f6c9af6
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2023-09-19 17:34:31 +0300

    Update 1_ocp-pre.sh

--- bastion/1_ocp-pre.sh
+++ bastion/1_ocp-pre.sh
@@ -7,12 +7,12 @@ OCP_VERSION="4.10.66"
 SSH_KEY_NAME="ocp4upi"
 
 # Download the OpenShift tools
-wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$OCP_VERSION/openshift-client-linux-$OCP_VERSION.tar.gz -P /tmp
-wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz -P /tmp
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$OCP_VERSION/openshift-client-linux-$OCP_VERSION.tar.gz -P $NBE_HOME
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz -P $NBE_HOME
 
 # Extract the OpenShift tools
-tar xvf /tmp/openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
-tar xvf /tmp/openshift-install-linux-$OCP_VERSION.tar.gz -C /usr/bin/
+tar xvf $NBE_HOME/openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
+tar xvf $NBE_HOME/openshift-install-linux-$OCP_VERSION.tar.gz -C /usr/bin/
 
 # Generate the SSH key
 ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/$SSH_KEY_NAME
