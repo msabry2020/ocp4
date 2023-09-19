fd5c8a4027e5d6b24de277f8282d003e82467b87
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2023-09-19 17:23:03 +0300

    Update 3_mirror_images.sh

--- bastion/3_mirror_images.sh
+++ bastion/3_mirror_images.sh
@@ -2,10 +2,10 @@
 
 set -x
 # Set the variables
+NBE_HOME='/nbe'
 BASE_DOMAIN='nbe.ahly.bank'
 CLUSTER_NAME='plz-vmware-sit-c01'
 INIT_PASSWORD='RWp4iYc2jeHNjGNWZxhT/Tu5nLs='
-NBE_HOME='/nbe'
 OCP_RELEASE='4.10.66'
 LOCAL_REGISTRY='registry.plz-vmware-sit-c01.nbe.ahly.bank:8443'
 LOCAL_REPOSITORY=plz-vmware-sit-c01/openshift4
@@ -20,6 +20,6 @@ LOCAL_REGISTRY_SECRET="auths\":{\"registry.${CLUSTER_NAME}.${BASE_DOMAIN}:8443\"
 # Log in to the registry as the init user
 podman login -u init -p $INIT_PASSWORD $LOCAL_REGISTRY 
 
-sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" pull-secret | jq . > $LOCAL_SECRET_JSON 
+sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" $NBE_HOME/pull-secret | jq . > $LOCAL_SECRET_JSON 
 
 oc adm release mirror -a ${LOCAL_SECRET_JSON} --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE}
