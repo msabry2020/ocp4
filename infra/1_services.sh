85e4943c81f595b4a988127c406daff59aee2a67
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2023-10-18 10:50:41 +0300

    Update 1_services.sh

--- infra/1_services.sh
+++ infra/1_services.sh
@@ -1,67 +1,85 @@
 #!/bin/bash
+set -x
+echo '# Set the variables #'
+BASE_DOMAIN='lab.local'
+CLUSTER_NAME='ocp412'
+REVERSE_SUBNET='50.168.192'
+INFRA_IP="192.168.50.163"
+BOOTSTRAP_IP="192.168.50.154"
+MASTER01_IP="192.168.50.155"
+MASTER02_IP="192.168.50.156"
+MASTER03_IP="192.168.50.157"
+WORKER01_IP="192.168.50.158"
+WORKER02_IP="192.168.50.159"
+WORKER03_IP="192.168.50.160"
+echo "-----------------------------------"
+set +x
 
-# Set the variables
-BASE_DOMAIN='nbe.com.eg'
-CLUSTER_NAME='cp4d'
-SUBNET='192.168.50'
-INFRA_IP="${SUBNET}.111"
-REGISTRY_IP="${SUBNET}.222"
-LB_OCT=$(echo $INFRA_IP | awk -F. '{print $4}')
-MAC='00:05:69:00:00'
-DISK='sda'
-
-# Backup existing config files
-cp /var/lib/tftpboot/pxelinux.cfg/default /var/lib/tftpboot/pxelinux.cfg/default.bkp
-cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bkp
-cp /etc/named.conf /etc/named.conf.bkp
-cp /var/named/${BASE_DOMAIN}.zone /var/named/${BASE_DOMAIN}.zone.bkp
-cp /var/named/${BASE_DOMAIN}.reverse.zone /var/named/${BASE_DOMAIN}.reverse.zone.bkp
-
-# PXE # 
-sed -i "s/HTTP_SERVER_IP/${INFRA_IP}/g" default
-sed -i "s/DISK/${DISK}/g" default
-mkdir /var/lib/tftpboot/pxelinux.cfg
-cp default /var/lib/tftpboot/pxelinux.cfg
-cp -r /usr/share/syslinux/* /var/lib/tftpboot
-
-# DHCP #
-sed -i "s/MAC/${MAC}/g" dhcpd.conf
-sed -i "s/GW/${INFRA_IP}/g" dhcpd.conf
-sed -i "s/DNS/${INFRA_IP}/g" dhcpd.conf
-sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" dhcpd.conf
-sed -i "s/SUBNET/${SUBNET}/g" dhcpd.conf
-sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" dhcpd.conf
-cp dhcpd.conf /etc/dhcp/dhcpd.conf 
-
-# DNS #
+echo '# DNS Configuration #'
+set -x
+## named.conf ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" named.conf 
-sed -i "s/SUBNET/${SUBNET}/g" forward.zone 
+sed -i "s/REVERSE_SUBNET/${REVERSE_SUBNET}/g" named.conf 
+
+## forward.zone ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" forward.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" forward.zone
 sed -i "s/INFRA_IP/${INFRA_IP}/g" forward.zone
-sed -i "s/REGISTRY_IP/${REGISTRY_IP}/g" forward.zone
-sed -i "s/LB_OCT/$LB_OCT/g" reverse.zone
+sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" forward.zone
+sed -i "s/MASTER01_IP/${MASTER01_IP}/g" forward.zone
+sed -i "s/MASTER02_IP/${MASTER02_IP}/g" forward.zone
+sed -i "s/MASTER03_IP/${MASTER03_IP}/g" forward.zone
+sed -i "s/WORKER01_IP/${WORKER01_IP}/g" forward.zone
+sed -i "s/WORKER02_IP/${WORKER02_IP}/g" forward.zone
+sed -i "s/WORKER03_IP/${WORKER03_IP}/g" forward.zone
+
+## reverse.zone ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" reverse.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" reverse.zone
+sed -i "s/INFRA/$(echo $INFRA_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/BOOTSTRAP/$(echo $BOOTSTRAP_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/MASTER01/$(echo $MASTER01_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/MASTER02/$(echo $MASTER02_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/MASTER03/$(echo $MASTER03_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER01/$(echo $WORKER01_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER02/$(echo $WORKER02_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER03/$(echo $WORKER03_IP | awk -F. '{print $4}')/g" reverse.zone
+
 cp named.conf /etc/named.conf
 cp forward.zone /var/named/${BASE_DOMAIN}.zone 
 cp reverse.zone /var/named/${BASE_DOMAIN}.reverse.zone
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# HAProxy #
+
+echo '# HAProxy Configuration #'
+set -x
+sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" haproxy.cfg
+sed -i "s/MASTER01_IP/${MASTER01_IP}/g" haproxy.cfg
+sed -i "s/MASTER02_IP/${MASTER02_IP}/g" haproxy.cfg
+sed -i "s/MASTER03_IP/${MASTER03_IP}/g" haproxy.cfg
+sed -i "s/WORKER01_IP/${WORKER01_IP}/g" haproxy.cfg
+sed -i "s/WORKER02_IP/${WORKER02_IP}/g" haproxy.cfg
+
 cp haproxy.cfg /etc/haproxy/haproxy.cfg 
 semanage boolean -m --on haproxy_connect_any
 setsebool -P haproxy_connect_any=1
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# HTTPD # 
+echo '# HTTP Server Configuration #'
+set -x
 cp httpd.conf /etc/httpd/conf/httpd.conf
-mkdir -p /var/www/html/openshift4/images
 mkdir -p /var/www/html/openshift4/ignitions
 restorecon -Rv /var/www/html/openshift4
+set +x
+sleep 5
+echo "-----------------------------------"
 
-# Start and Enable Services #
-systemctl enable --now dhcpd
+echo # Start and Enable Services #
+set -x
 systemctl enable --now named
-systemctl enable --now tftp
 systemctl enable --now haproxy
 systemctl enable --now httpd
-
