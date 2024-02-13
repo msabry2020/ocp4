021b38c1e787496f59f0cd76a7d70d7510055a8a
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2024-02-13 22:37:50 +0200

    Update 3_mirror_images.sh

--- bastion/3_mirror_images.sh
+++ bastion/3_mirror_images.sh
@@ -2,25 +2,25 @@
 
 set -x
 # Set the variables
-NBE_HOME='/nbe'
-BASE_DOMAIN='nbe.ahly.bank'
-CLUSTER_NAME='plz-vmware-sit-c01'
-INIT_PASSWORD=$(cat $NBE_HOME/quay-install/init_password.txt)
-OCP_RELEASE='4.10.66'
-LOCAL_REGISTRY='registry.plz-vmware-sit-c01.nbe.ahly.bank:8443'
-LOCAL_REPOSITORY=plz-vmware-sit-c01/openshift4
+INSTALL_HOME='/opt/install'
+BASE_DOMAIN='lab.local'
+CLUSTER_NAME='ocp4'
+INIT_PASSWORD=$(cat $INSTALL_HOME/quay-install/init_password.txt)
+OCP_RELEASE='4.12.30'
+LOCAL_REGISTRY='registry.ocp4.lab.local:8443'
+LOCAL_REPOSITORY=openshift4
 PRODUCT_REPO='openshift-release-dev'
 RELEASE_NAME="ocp-release"
-LOCAL_SECRET_JSON="$NBE_HOME/pull-secret.json"
+LOCAL_SECRET_JSON="$INSTALL_HOME/pull-secret.json"
 ARCHITECTURE=x86_64
 REGISTRY_TOKEN=$(echo -n "init:${INIT_PASSWORD}" | base64 -w0)
 LOCAL_REGISTRY_SECRET="auths\":{\"registry.${CLUSTER_NAME}.${BASE_DOMAIN}:8443\":{\"auth\":\"${REGISTRY_TOKEN}\",\"email\":\"admin@${BASE_DOMAIN}\"},"
 
-echo $REGISTRY_TOKEN > $NBE_HOME/quay-install/registry_token.64
+echo $REGISTRY_TOKEN > $INSTALL_HOME/quay-install/registry_token.64
 
 # Log in to the registry as the init user
 podman login -u init -p $INIT_PASSWORD $LOCAL_REGISTRY 
 
-sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" $NBE_HOME/pull-secret | jq . > $LOCAL_SECRET_JSON 
+sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" $INSTALL_HOME/pull-secret | jq . > $LOCAL_SECRET_JSON 
 
 oc adm release mirror -a ${LOCAL_SECRET_JSON} --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE}
