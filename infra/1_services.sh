bc8249586ce3dd03981b2a2b04a453c4e0206419
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2024-02-13 18:55:41 +0200

    Update 1_services.sh

--- infra/1_services.sh
+++ infra/1_services.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 set -x
 echo '# Set the variables #'
-BASE_DOMAIN='nbe.ahly.bank'
-CLUSTER_NAME='plz-vmware-sit-c01'
-REVERSE_SUBNET='162.50.10'
-INFRA_IP="10.50.162.163"
-BASTION_IP="10.50.162.152"
-STORAGE_IP="10.50.162.162"
-WAS_IP="10.50.162.161"
-BOOTSTRAP_IP="10.50.162.154"
-MASTER01_IP="10.50.162.155"
-MASTER02_IP="10.50.162.156"
-MASTER03_IP="10.50.162.157"
-WORKER01_IP="10.50.162.158"
-WORKER02_IP="10.50.162.159"
-WORKER03_IP="10.50.162.160"
+echo '# Set the variables #'
+BASE_DOMAIN='lab.local'
+CLUSTER_NAME='ocp4'
+SUBNET="10.11.31"
+REVERSE_SUBNET='31.11.10'
+UTIL_IP="10.11.31.200"
+CONTROL01_IP="10.11.31.201"
+CONTROL02_IP="10.11.31.202"
+CONTROL03_IP="10.11.31.203"
+INFRA01_IP="10.11.31.204"
+INFRA02_IP="10.11.31.205"
+INFRA03_IP="10.11.31.206"
+WORKER01_IP="10.11.31.207"
+WORKER02_IP="10.11.31.208"
+WORKER03_IP="10.11.31.209"
+BOOTSTRAP_IP="10.11.31.210"
+MAC="56:6f:c4:78:00:0"
+BASTION_IP="10.11.31.200"
+DISK='vda'
+#STORAGE_IP="10.11.31.200"
+#WAS_IP="10.11.31.200"
+
 echo "-----------------------------------"
 set +x
 
@@ -27,32 +35,38 @@ sed -i "s/REVERSE_SUBNET/${REVERSE_SUBNET}/g" named.conf
 ## forward.zone ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" forward.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" forward.zone
-sed -i "s/INFRA_IP/${INFRA_IP}/g" forward.zone
-sed -i "s/BASTION_IP/${BASTION_IP}/g" forward.zone
-sed -i "s/STORAGE_IP/${STORAGE_IP}/g" forward.zone
-sed -i "s/WAS_IP/${WAS_IP}/g" forward.zone
+sed -i "s/UTIL_IP/${UTIL_IP}/g" forward.zone
 sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" forward.zone
-sed -i "s/MASTER01_IP/${MASTER01_IP}/g" forward.zone
-sed -i "s/MASTER02_IP/${MASTER02_IP}/g" forward.zone
-sed -i "s/MASTER03_IP/${MASTER03_IP}/g" forward.zone
+sed -i "s/CONTROL01_IP/${CONTROL01_IP}/g" forward.zone
+sed -i "s/CONTROL02_IP/${CONTROL02_IP}/g" forward.zone
+sed -i "s/CONTROL03_IP/${CONTROL03_IP}/g" forward.zone
+sed -i "s/INFRA01_IP/${INFRA01_IP}/g" forward.zone
+sed -i "s/INFRA02_IP/${INFRA02_IP}/g" forward.zone
+sed -i "s/INFRA03_IP/${INFRA03_IP}/g" forward.zone
 sed -i "s/WORKER01_IP/${WORKER01_IP}/g" forward.zone
 sed -i "s/WORKER02_IP/${WORKER02_IP}/g" forward.zone
 sed -i "s/WORKER03_IP/${WORKER03_IP}/g" forward.zone
+sed -i "s/BASTION_IP/${BASTION_IP}/g" forward.zone
+#sed -i "s/STORAGE_IP/${STORAGE_IP}/g" forward.zone
+#sed -i "s/WAS_IP/${WAS_IP}/g" forward.zone
 
 ## reverse.zone ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" reverse.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" reverse.zone
-sed -i "s/INFRA/$(echo $INFRA_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/BASTION/$(echo $BASTION_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/STORAGE/$(echo $STORAGE_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/WAS/$(echo $WAS_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/UTIL/$(echo $UTIL_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/BOOTSTRAP/$(echo $BOOTSTRAP_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/MASTER01/$(echo $MASTER01_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/MASTER02/$(echo $MASTER02_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/MASTER03/$(echo $MASTER03_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL01/$(echo $CONTROL01_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL02/$(echo $CONTROL02_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL03/$(echo $CONTROL03_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA01/$(echo $INFRA01_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA02/$(echo $INFRA02_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA03/$(echo $INFRA03_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/WORKER01/$(echo $WORKER01_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/WORKER02/$(echo $WORKER02_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/WORKER03/$(echo $WORKER03_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/BASTION/$(echo $BASTION_IP | awk -F. '{print $4}')/g" reverse.zone
+#sed -i "s/STORAGE/$(echo $STORAGE_IP | awk -F. '{print $4}')/g" reverse.zone
+#sed -i "s/WAS/$(echo $WAS_IP | awk -F. '{print $4}')/g" reverse.zone
 
 cp named.conf /etc/named.conf
 cp forward.zone /var/named/${BASE_DOMAIN}.zone 
@@ -65,12 +79,15 @@ echo "-----------------------------------"
 echo '# HAProxy Configuration #'
 set -x
 sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" haproxy.cfg
-sed -i "s/MASTER01_IP/${MASTER01_IP}/g" haproxy.cfg
-sed -i "s/MASTER02_IP/${MASTER02_IP}/g" haproxy.cfg
-sed -i "s/MASTER03_IP/${MASTER03_IP}/g" haproxy.cfg
+sed -i "s/CONTROL01_IP/${CONTROL01_IP}/g" haproxy.cfg
+sed -i "s/CONTROL02_IP/${CONTROL02_IP}/g" haproxy.cfg
+sed -i "s/CONTROL03_IP/${CONTROL03_IP}/g" haproxy.cfg
 sed -i "s/WORKER01_IP/${WORKER01_IP}/g" haproxy.cfg
 sed -i "s/WORKER02_IP/${WORKER02_IP}/g" haproxy.cfg
 sed -i "s/WORKER03_IP/${WORKER03_IP}/g" haproxy.cfg
+sed -i "s/INFRA01_IP/${INFRA01_IP}/g" haproxy.cfg
+sed -i "s/INFRA02_IP/${INFRA02_IP}/g" haproxy.cfg
+sed -i "s/INFRA03_IP/${INFRA03_IP}/g" haproxy.cfg
 
 cp haproxy.cfg /etc/haproxy/haproxy.cfg 
 semanage boolean -m --on haproxy_connect_any
@@ -82,15 +99,38 @@ echo "-----------------------------------"
 echo '# HTTP Server Configuration #'
 set -x
 cp httpd.conf /etc/httpd/conf/httpd.conf
-mkdir -p /var/www/html/openshift4/ignitions
-restorecon -Rv /var/www/html/openshift4
+mkdir -p /var/www/html/openshift4
+#restorecon -Rv /var/www/html/openshift4
+set +x
+sleep 5
+echo "-----------------------------------"
+
+echo '# PXE Server Configuration #'
+sed -i "s/HTTP_SERVER_IP/${UTIL_IP}/g" default
+sed -i "s/DISK/${DISK}/g" default
+mkdir /var/lib/tftpboot/pxelinux.cfg
+cp default /var/lib/tftpboot/pxelinux.cfg
+cp -r /usr/share/syslinux/* /var/lib/tftpboot
+set +x
+sleep 5
+echo "-----------------------------------"
+
+echo '# DHCP Server Configuration #'
+sed -i "s/MAC/${MAC}/g" dhcpd.conf
+sed -i "s/GW/${UTIL_IP}/g" dhcpd.conf
+sed -i "s/DNS/${UTIL_IP}/g" dhcpd.conf
+sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" dhcpd.conf
+sed -i "s/SUBNET/${SUBNET}/g" dhcpd.conf
+sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" dhcpd.conf
+cp dhcpd.conf /etc/dhcp/dhcpd.conf 
 set +x
 sleep 5
 echo "-----------------------------------"
 
 echo # Start and Enable Services #
 set -x
+systemctl enable --now dhcpd
 systemctl enable --now named
+systemctl enable --now tftp
 systemctl enable --now haproxy
 systemctl enable --now httpd
-
