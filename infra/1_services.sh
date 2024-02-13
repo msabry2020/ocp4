007d7ff72beee58c311c246dd809b511e4c97bd2
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2024-02-13 19:18:52 +0200

    Update 1_services.sh

--- infra/1_services.sh
+++ infra/1_services.sh
@@ -7,15 +7,15 @@ CLUSTER_NAME='ocp4'
 SUBNET="10.11.31"
 REVERSE_SUBNET='31.11.10'
 UTIL_IP="10.11.31.200"
-CONTROL01_IP="10.11.31.201"
-CONTROL02_IP="10.11.31.202"
-CONTROL03_IP="10.11.31.203"
-INFRA01_IP="10.11.31.204"
-INFRA02_IP="10.11.31.205"
-INFRA03_IP="10.11.31.206"
-WORKER01_IP="10.11.31.207"
-WORKER02_IP="10.11.31.208"
-WORKER03_IP="10.11.31.209"
+CONTROL1_IP="10.11.31.201"
+CONTROL2_IP="10.11.31.202"
+CONTROL3_IP="10.11.31.203"
+INFRA1_IP="10.11.31.204"
+INFRA2_IP="10.11.31.205"
+INFRA3_IP="10.11.31.206"
+WORKER1_IP="10.11.31.207"
+WORKER2_IP="10.11.31.208"
+WORKER3_IP="10.11.31.209"
 BOOTSTRAP_IP="10.11.31.210"
 MAC="56:6f:c4:78:00:0"
 BASTION_IP="10.11.31.200"
@@ -36,15 +36,15 @@ sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" forward.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" forward.zone
 sed -i "s/UTIL_IP/${UTIL_IP}/g" forward.zone
 sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" forward.zone
-sed -i "s/CONTROL01_IP/${CONTROL01_IP}/g" forward.zone
-sed -i "s/CONTROL02_IP/${CONTROL02_IP}/g" forward.zone
-sed -i "s/CONTROL03_IP/${CONTROL03_IP}/g" forward.zone
-sed -i "s/INFRA01_IP/${INFRA01_IP}/g" forward.zone
-sed -i "s/INFRA02_IP/${INFRA02_IP}/g" forward.zone
-sed -i "s/INFRA03_IP/${INFRA03_IP}/g" forward.zone
-sed -i "s/WORKER01_IP/${WORKER01_IP}/g" forward.zone
-sed -i "s/WORKER02_IP/${WORKER02_IP}/g" forward.zone
-sed -i "s/WORKER03_IP/${WORKER03_IP}/g" forward.zone
+sed -i "s/CONTROL1_IP/${CONTROL1_IP}/g" forward.zone
+sed -i "s/CONTROL2_IP/${CONTROL2_IP}/g" forward.zone
+sed -i "s/CONTROL3_IP/${CONTROL3_IP}/g" forward.zone
+sed -i "s/INFRA1_IP/${INFRA1_IP}/g" forward.zone
+sed -i "s/INFRA2_IP/${INFRA2_IP}/g" forward.zone
+sed -i "s/INFRA3_IP/${INFRA3_IP}/g" forward.zone
+sed -i "s/WORKER1_IP/${WORKER1_IP}/g" forward.zone
+sed -i "s/WORKER2_IP/${WORKER2_IP}/g" forward.zone
+sed -i "s/WORKER3_IP/${WORKER3_IP}/g" forward.zone
 sed -i "s/BASTION_IP/${BASTION_IP}/g" forward.zone
 sed -i "s/STORAGE_IP/${STORAGE_IP}/g" forward.zone
 
@@ -53,15 +53,15 @@ sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" reverse.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" reverse.zone
 sed -i "s/UTIL/$(echo $UTIL_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/BOOTSTRAP/$(echo $BOOTSTRAP_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/CONTROL01/$(echo $CONTROL01_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/CONTROL02/$(echo $CONTROL02_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/CONTROL03/$(echo $CONTROL03_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/INFRA01/$(echo $INFRA01_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/INFRA02/$(echo $INFRA02_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/INFRA03/$(echo $INFRA03_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/WORKER01/$(echo $WORKER01_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/WORKER02/$(echo $WORKER02_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/WORKER03/$(echo $WORKER03_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL1/$(echo $CONTROL1_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL2/$(echo $CONTROL2_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/CONTROL3/$(echo $CONTROL3_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA1/$(echo $INFRA1_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA2/$(echo $INFRA2_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/INFRA3/$(echo $INFRA3_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER1/$(echo $WORKER1_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER2/$(echo $WORKER2_IP | awk -F. '{print $4}')/g" reverse.zone
+sed -i "s/WORKER3/$(echo $WORKER3_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/BASTION/$(echo $BASTION_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/STORAGE/$(echo $STORAGE_IP | awk -F. '{print $4}')/g" reverse.zone
 
@@ -76,15 +76,15 @@ echo "-----------------------------------"
 echo '# HAProxy Configuration #'
 set -x
 sed -i "s/BOOTSTRAP_IP/${BOOTSTRAP_IP}/g" haproxy.cfg
-sed -i "s/CONTROL01_IP/${CONTROL01_IP}/g" haproxy.cfg
-sed -i "s/CONTROL02_IP/${CONTROL02_IP}/g" haproxy.cfg
-sed -i "s/CONTROL03_IP/${CONTROL03_IP}/g" haproxy.cfg
-sed -i "s/WORKER01_IP/${WORKER01_IP}/g" haproxy.cfg
-sed -i "s/WORKER02_IP/${WORKER02_IP}/g" haproxy.cfg
-sed -i "s/WORKER03_IP/${WORKER03_IP}/g" haproxy.cfg
-sed -i "s/INFRA01_IP/${INFRA01_IP}/g" haproxy.cfg
-sed -i "s/INFRA02_IP/${INFRA02_IP}/g" haproxy.cfg
-sed -i "s/INFRA03_IP/${INFRA03_IP}/g" haproxy.cfg
+sed -i "s/CONTROL1_IP/${CONTROL1_IP}/g" haproxy.cfg
+sed -i "s/CONTROL2_IP/${CONTROL2_IP}/g" haproxy.cfg
+sed -i "s/CONTROL3_IP/${CONTROL3_IP}/g" haproxy.cfg
+sed -i "s/WORKER1_IP/${WORKER1_IP}/g" haproxy.cfg
+sed -i "s/WORKER2_IP/${WORKER2_IP}/g" haproxy.cfg
+sed -i "s/WORKER3_IP/${WORKER3_IP}/g" haproxy.cfg
+sed -i "s/INFRA1_IP/${INFRA1_IP}/g" haproxy.cfg
+sed -i "s/INFRA2_IP/${INFRA2_IP}/g" haproxy.cfg
+sed -i "s/INFRA3_IP/${INFRA3_IP}/g" haproxy.cfg
 
 cp haproxy.cfg /etc/haproxy/haproxy.cfg 
 semanage boolean -m --on haproxy_connect_any
