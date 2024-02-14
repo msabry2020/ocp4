92951466d03cc1a723fe3bddcbe90877b8ed8266
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2024-02-14 16:44:31 -0500

    export no_proxy

--- bastion/3_mirror_images.sh
+++ bastion/3_mirror_images.sh
@@ -16,11 +16,13 @@ REGISTRY_TOKEN=$(echo -n "init:${INIT_PASSWORD}" | base64 -w0)
 LOCAL_REGISTRY_SECRET="auths\":{\"registry.${CLUSTER_NAME}.${BASE_DOMAIN}:8443\":{\"auth\":\"${REGISTRY_TOKEN}\",\"email\":\"admin@${BASE_DOMAIN}\"},"
 
 echo $REGISTRY_TOKEN > $INSTALL_HOME/quay-install/registry_token.64
+set -x
+
+export no_proxy=registry.$CLUSTER_NAME.$BASE_DOMAIN
 
 # Log in to the registry as the init user
 podman login -u init -p $INIT_PASSWORD $LOCAL_REGISTRY 
 
 sed "s|auths\":{|${LOCAL_REGISTRY_SECRET}|g" $INSTALL_HOME/pull-secret | jq . > $LOCAL_SECRET_JSON 
 
-set -x
 oc adm release mirror -a ${LOCAL_SECRET_JSON} --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE}
