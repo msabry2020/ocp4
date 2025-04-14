4edeaf7b3257b0534186701806c0d6f53701f542
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-04-14 13:23:14 +0200

    edit 1_services.sh

--- utility/1_services.sh
+++ utility/1_services.sh
@@ -22,9 +22,7 @@ sed -i "s/MASTER02_IP/${MASTER02_IP}/g" forward.zone
 sed -i "s/MASTER03_IP/${MASTER03_IP}/g" forward.zone
 sed -i "s/WORKER01_IP/${WORKER01_IP}/g" forward.zone
 sed -i "s/WORKER02_IP/${WORKER02_IP}/g" forward.zone
-sed -i "s/WORKER03_IP/${WORKER03_IP}/g" forward.zone
 sed -i "s/BASTION_IP/${BASTION_IP}/g" forward.zone
-sed -i "s/STORAGE_IP/${STORAGE_IP}/g" forward.zone
 ## reverse.zone ##
 sed -i "s/BASE_DOMAIN/${BASE_DOMAIN}/g" reverse.zone
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" reverse.zone
@@ -35,9 +33,7 @@ sed -i "s/MASTER02/$(echo $MASTER02_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/MASTER03/$(echo $MASTER03_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/WORKER01/$(echo $WORKER01_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/WORKER02/$(echo $WORKER02_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/WORKER03/$(echo $WORKER03_IP | awk -F. '{print $4}')/g" reverse.zone
 sed -i "s/BASTION/$(echo $BASTION_IP | awk -F. '{print $4}')/g" reverse.zone
-sed -i "s/STORAGE/$(echo $STORAGE_IP | awk -F. '{print $4}')/g" reverse.zone
 ## named.conf ##
 cp named.conf /etc/named.conf
 cp forward.zone /var/named/${BASE_DOMAIN}.zone 
@@ -51,7 +47,6 @@ sed -i "s/MASTER02_IP/${MASTER02_IP}/g" haproxy.cfg
 sed -i "s/MASTER03_IP/${MASTER03_IP}/g" haproxy.cfg
 sed -i "s/WORKER01_IP/${WORKER01_IP}/g" haproxy.cfg
 sed -i "s/WORKER02_IP/${WORKER02_IP}/g" haproxy.cfg
-sed -i "s/WORKER03_IP/${WORKER03_IP}/g" haproxy.cfg
 cp haproxy.cfg /etc/haproxy/haproxy.cfg 
 semanage boolean -m --on haproxy_connect_any
 setsebool -P haproxy_connect_any=1
@@ -73,7 +68,6 @@ sed -i "s/MASTER02_IP/${MASTER02_IP}/g" dhcpd.conf
 sed -i "s/MASTER03_IP/${MASTER03_IP}/g" dhcpd.conf
 sed -i "s/WORKER01_IP/${WORKER01_IP}/g" dhcpd.conf
 sed -i "s/WORKER02_IP/${WORKER02_IP}/g" dhcpd.conf
-sed -i "s/WORKER03_IP/${WORKER03_IP}/g" dhcpd.conf
 sed -i "s/SUBNET/${SUBNET}/g" dhcpd.conf
 sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" dhcpd.conf
 cp dhcpd.conf /etc/dhcp/dhcpd.conf 
