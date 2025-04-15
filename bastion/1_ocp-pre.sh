7bb077a15cbedf1fd0d05af9bfbb48df6ac28c13
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-04-15 10:20:20 +0200

    wget -nc

--- bastion/1_ocp-pre.sh
+++ bastion/1_ocp-pre.sh
@@ -7,8 +7,8 @@ echo -e "Create INSTALL_HOME\n"
 mkdir -p $INSTALL_HOME
 
 echo -e "Download the OpenShift tools\n"
-wget -c --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/openshift-client-linux-$OCP_VERSION.tar.gz -P /tmp
-wget -c --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz -P /tmp
+wget -nc --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/openshift-client-linux-$OCP_VERSION.tar.gz -P /tmp
+wget -nc --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz -P /tmp
 
 echo -e "Extract the OpenShift tools\n"
 tar xvf --skip-old-files /tmp/openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
@@ -16,9 +16,9 @@ tar xvf --skip-old-files /tmp/openshift-install-linux-$OCP_VERSION.tar.gz -C /us
 
 echo -e "Download the OpenShift images\n"
 mkdir -p /tmp/images
-wget -c --no-check-certificate $RHCOS_URL/rhcos-live-kernel-x86_64 -O /tmp/images/rhcos-live-kernel-x86_64
-wget -c --no-check-certificate $RHCOS_URL/rhcos-live-initramfs.x86_64.img -O /tmp/images/rhcos-live-initramfs.x86_64.img
-wget -c --no-check-certificate $RHCOS_URL/rhcos-live-rootfs.x86_64.img -O /tmp/images/rhcos-live-rootfs.x86_64.img
+wget -nc --no-check-certificate $RHCOS_URL/rhcos-live-kernel-x86_64 -O /tmp/images/rhcos-live-kernel-x86_64
+wget -nc --no-check-certificate $RHCOS_URL/rhcos-live-initramfs.x86_64.img -O /tmp/images/rhcos-live-initramfs.x86_64.img
+wget -nc --no-check-certificate $RHCOS_URL/rhcos-live-rootfs.x86_64.img -O /tmp/images/rhcos-live-rootfs.x86_64.img
 
 echo -e "Copy the OpenShift images to the infra node\n"
 rsync -av /tmp/images root@$UTIL_IP:/var/www/html/openshift4/
