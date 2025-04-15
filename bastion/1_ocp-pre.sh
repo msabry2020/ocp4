eb0053207680c1cf6c66651595b05c895136c0de
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-04-15 10:26:55 +0200

    edit ocp_pre

--- bastion/1_ocp-pre.sh
+++ bastion/1_ocp-pre.sh
@@ -11,8 +11,8 @@ wget -nc --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/opensh
 wget -nc --no-check-certificate $BASE_URL/x86_64/clients/ocp/$OCP_VERSION/openshift-install-linux-$OCP_VERSION.tar.gz -P /tmp
 
 echo -e "Extract the OpenShift tools\n"
-tar xvf --skip-old-files /tmp/openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
-tar xvf --skip-old-files /tmp/openshift-install-linux-$OCP_VERSION.tar.gz -C /usr/bin/
+tar --skip-old-files -xvf /tmp/openshift-client-linux-$OCP_VERSION.tar.gz -C /usr/bin/
+tar --skip-old-files -xvf /tmp/openshift-install-linux-$OCP_VERSION.tar.gz -C /usr/bin/
 
 echo -e "Download the OpenShift images\n"
 mkdir -p /tmp/images
@@ -21,7 +21,7 @@ wget -nc --no-check-certificate $RHCOS_URL/rhcos-live-initramfs.x86_64.img -O /t
 wget -nc --no-check-certificate $RHCOS_URL/rhcos-live-rootfs.x86_64.img -O /tmp/images/rhcos-live-rootfs.x86_64.img
 
 echo -e "Copy the OpenShift images to the infra node\n"
-rsync -av /tmp/images root@$UTIL_IP:/var/www/html/openshift4/
+rsync --ignore-existing -av /tmp/images root@$UTIL_IP:/var/www/html/openshift4/
 
 echo -e "Generate the SSH key\n"
 ssh-keygen -t rsa -b 4096 -N '' -f $INSTALL_HOME/$SSH_KEY_NAME
